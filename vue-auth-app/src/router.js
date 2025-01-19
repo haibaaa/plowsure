9d68b7a1b67450feeb546ec41187445cfedc2d12
haibaaa <9700samarth@gmail.com>
2025-01-19 08:52:24 +0530

    p

--- vue-auth-app/src/router.js
+++ vue-auth-app/src/router.js
@@ -4,12 +4,16 @@ import { useAuth } from './composables/useAuth'
 import LoginForm from './components/LoginForm.vue'
 import RegisterForm from './components/RegisterForm.vue'
 import Dashboard from './components/Dashboard.vue'
+import Policypage from './components/Policypage.vue'
+import Calculator from './components/Calculator.vue'
 
 const routes = [
-  { path: '/', redirect: '/login' },
+  { path: '/', redirect: '/dashboard' },
   { path: '/login', component: LoginForm },
   { path: '/register', component: RegisterForm },
-  { path: '/dashboard', component: Dashboard }
+  { path: '/dashboard', component: Dashboard },
+  { path: '/policy', component: Policypage},
+  { path: '/calculator', component: Calculator}
 ]
 
 const router = createRouter({
